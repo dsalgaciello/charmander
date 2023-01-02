48e04b141831893a05ca061d4a9779378a7e493f
Darwin Salgaciello <dsalgaciello@cadence-labs.com>
2023-01-02 15:37:14 -0500

    Add links

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,11 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+      <>
+          <h1 className="title">{title}</h1>
+          <div>
+            <a href={"/"}>Home</a> &nbsp;
+            <a href={"/videos"}>Videos</a>
+          </div>
+      </>
+  )
 }
